d0f122e239ac14139ee1a491fcbc05c3508aabba
Wellerson Lopes <well@edu.unifor.br>
2022-05-31 22:34:24 -0300

    Fish testimonials pagination carousel

--- main.js
+++ main.js
@@ -29,3 +29,13 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
